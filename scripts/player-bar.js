a8023d39b6f606b2c67ddcee8058242013224a2b
Emily <flynns_shutters@hotmail.com>
2019-04-05 20:07:34 -0400

    checkpoint 11 done

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -1,44 +1,46 @@
 {
-  $('button#play-pause').on('click', function() {
-  player.playPause();
-  $(this).attr('playState', player.playState);
-});
+    $('button#play-pause').on('click', function() {
+        helper.playPauseAndUpdate();
+        $(this).attr('playState', player.playState);
+    });
 
-   $('button#next').on('click', function() {
-     if (player.playState !== 'playing') { return; }
-
-     const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-     const nextSongIndex = currentSongIndex + 1;
-     if (nextSongIndex >= album.songs.length) { return; }
-
-     const nextSong = album.songs[nextSongIndex];
-     player.playPause(nextSong);
-   });
-
-   $('button#previous').on('click', function() {
+    $('button#next').on('click', function() {
        if (player.playState !== 'playing') { return; }
 
        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
-       const prevSongIndex = currentSongIndex - 1;
-       if (prevSongIndex === -1) { return; }
+       const nextSongIndex = currentSongIndex + 1;
+       if (nextSongIndex >= album.songs.length) { return; }
 
+       const nextSong = album.songs[nextSongIndex];
+       helper.playPauseAndUpdate(nextSong);
+    });
 
-       const prevSong = album.songs[prevSongIndex];
-       player.playPause(prevSong);
+    $('button#previous').on('click', function() {
+        if (player.playState !== 'playing') { return; }
 
-     });
+        const currentSongIndex = album.songs.indexOf(player.currentlyPlaying);
+        const prevSongIndex = currentSongIndex - 1;
+        if (prevSongIndex < 0) { return; }
 
-   $('#time-control input').on('input', function (event) {
-     player.skipTo(event.target.value);
-   });
+        const prevSong = album.songs[prevSongIndex];
+        helper.playPauseAndUpdate(prevSong);
+    });
 
+    $('#time-control input').on('input', function (event) {
+        player.skipTo(event.target.value);
+    });
 
-   setInterval( () => {
-    if (player.playState !== 'playing') { return; }
-    const currentTime = player.getTime();
-    const duration = player.getDuration();
-    const percent = (currentTime / duration) * 100;
-    $('#time-control .current-time').text( currentTime );
-    $('#time-control input').val(percent);
-   }, 1000);
-   }
+    setInterval( () => {
+        if (player.playState !== 'playing') { return; }
+        let currentTime = player.getTime();
+        const duration = player.getDuration();
+        const percent = (currentTime / duration) * 100;
+        currentTime = player.prettyTime(currentTime);
+        $('#time-control .current-time').text( currentTime );
+        $('#time-control input').val(percent);
+    }, 1000);
+
+    $('#volume-control input').on('input', function (event) {
+        player.setVolume(event.target.value);
+    });
+}
